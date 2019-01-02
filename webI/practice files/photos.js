1b1bebcbb834dcfe99703537e26afb1dad23cbdf
arhswebdesign <45952368+arhswebdesign@users.noreply.github.com>
2019-01-02 12:58:47 -0500

    Add files via upload

---
+++ webI/practice files/photos.js
@@ -0,0 +1,72 @@
+WPGgallery = new Object();
+WPGgallery.name = "Adobe%20Web%20Photo%20Gallery";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "6/13/2012"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "2003-Kawasaki-Concoursa.jpg";
+gPhotos[0].ImageWidth = 350;
+gPhotos[0].ImageHeight = 250;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 53;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "2003-Kawasaki-Concoursa%20copy.jpg";
+gPhotos[1].ImageWidth = 350;
+gPhotos[1].ImageHeight = 250;
+gPhotos[1].ThumbWidth = 75;
+gPhotos[1].ThumbHeight = 53;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "2003-Kawasaki-ZG1000-A18-Red-1.jpg";
+gPhotos[2].ImageWidth = 350;
+gPhotos[2].ImageHeight = 248;
+gPhotos[2].ThumbWidth = 75;
+gPhotos[2].ThumbHeight = 53;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "2003-Kawasaki-ZG1000-Red-1.jpg";
+gPhotos[3].ImageWidth = 350;
+gPhotos[3].ImageHeight = 248;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 53;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "2003-Kawasaki-ZG1000-Red-2.jpg";
+gPhotos[4].ImageWidth = 350;
+gPhotos[4].ImageHeight = 248;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 53;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "2003-Kawasaki-ZG1000-Red-5.jpg";
+gPhotos[5].ImageWidth = 350;
+gPhotos[5].ImageHeight = 248;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 53;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "2003-Kawasaki-ZG1000-Red-small.jpg";
+gPhotos[6].ImageWidth = 350;
+gPhotos[6].ImageHeight = 247;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 53;
+gPhotos[6].meta = new Object();
+
