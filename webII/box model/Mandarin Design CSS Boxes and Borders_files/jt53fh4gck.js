2a6210731b712ac7c00f6e507ba40cfd5af99b6a
arhswebdesign <45952368+arhswebdesign@users.noreply.github.com>
2019-01-02 13:04:28 -0500

    Add files via upload

---
+++ webII/box model/Mandarin Design CSS Boxes and Borders_files/jt53fh4gck.js
@@ -0,0 +1,18 @@
+<!--
+drawTREmbed( '<div id="technorati">' +
+'<form id="te_s" method="post" action="http://www.technorati.com/search.php">' +
+'<label for="te_search">Search this blog:</label> <input id="te_search" type="text" size="12" name="s" maxlength="255" />' +
+'<input type=\"submit\" class=\"btn\" value=\"Search\" /> <input type=\"hidden\" name=\"sub\" value=\"searchlet\" /> <input type=\"hidden\" name=\"from\" value=\"http://www.mandarindesign.com/blogger.html\" /> <input type=\"hidden\" name=\"cc\" value=\"jt53fh4gck\" /> </form>' +
+'<p id="te_l" class="te_l">' +
+'<a href="http://www.technorati.com/search/http://www.mandarindesign.com/blogger.html?cc=jt53fh4gck" id="te_lh">&#187 Blogs that link here</a>' +
+'<br /><a href="http://www.technorati.com/profile/goodwool" id="te_vp">&#187; View my profile</a>' +
+'</p>' +
+'<p id="te_lo" class="te_lo"><a href="http://www.technorati.com/?cc=jt53fh4gck"><img border="0" src="http://static.technorati.com/pix/tn-tiny.gif" alt="Powered by Technorati" /></a></p>' +
+'</div>'
+);
+
+
+function drawTREmbed(s){
+    document.write(s);
+}
+// -->
