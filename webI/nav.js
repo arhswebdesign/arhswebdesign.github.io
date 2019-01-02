01189f24e695a7302d4e1feb6f0a347fb04988ce
arhswebdesign <45952368+arhswebdesign@users.noreply.github.com>
2019-01-02 12:22:40 -0500

    Add files via upload

---
+++ webI/nav.js
@@ -0,0 +1,5 @@
+
+document.write("<p class='header' ><a href='../../index.html' style='color:#91B9EC;'>Home</a> - <a href='../home.html' style='color:#91B9EC;'>Web Design I</a> - <a href='http://www.w3schools.com/default.asp' style='color:#91B9EC;'>W3Schools</a> - <a href='../xhtml/basicStructure.html' style='color:#91B9EC;'> Html Structure </a> - <a href='../general/color.html' style='color:#91B9EC;'>Thinking About Color</a> - <a href='../css/internal.html' style='color:#91B9EC;'>Internal Style</a> - <a href='../general/due_dates.html' style='color:#91B9EC;'>Due Dates</a> - <a href='../general/resources.html' style='color:#91B9EC;'>Resources</a> </p>")
+
+
+
